b83e88a18d31cc743284c2d0be153568ce04a03b
Georgios Kopanidis <kopanidis@me.com>
2023-12-25 14:20:33 +0000

    Initial commit filter method

--- filter.js
+++ filter.js
@@ -21,6 +21,13 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21)
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul")[0];
+console.log(paul);
+
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +62,8 @@ const students = [
     ]
   },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
